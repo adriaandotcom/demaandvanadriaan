50f2b4abfa66e300588264febf6cb9597ee78704
Adriaan van Rossum <git@adriaan.io>
2015-11-01 23:52:06 +0100

    Update menu dropdown icon to new Font Awesome icon

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -4,14 +4,14 @@
 var navigation = responsiveNav("#site-nav", { // Selector: The ID of the wrapper
   animate: true, // Boolean: Use CSS3 transitions, true or false
   transition: 400, // Integer: Speed of the transition, in milliseconds
-  label: "<i class='icon-reorder'></i> Menu", // String: Label for the navigation toggle
+  label: "<i class='fa fa-bars'></i> Menu", // String: Label for the navigation toggle
   insert: "before", // String: Insert the toggle before or after the navigation
   customToggle: "", // Selector: Specify the ID of a custom toggle
   openPos: "relative", // String: Position of the opened nav, relative or static
   jsClass: "js", // String: 'JS enabled' class which is added to <html> el
-  init: function(){}, // Function: Init callback
-  open: function(){}, // Function: Open callback
-  close: function(){} // Function: Close callback
+  init: function() {}, // Function: Init callback
+  open: function() {}, // Function: Open callback
+  close: function() {} // Function: Close callback
 });
 
 $('html').click(function() {
@@ -19,11 +19,11 @@ $('html').click(function() {
   navigation.toggle();
 });
 
-$('#site-nav').click(function(event){
-    event.stopPropagation();
+$('#site-nav').click(function(event) {
+  event.stopPropagation();
 });
 
 // FitVids options
 $(function() {
-	$("article").fitVids();
+  $("article").fitVids();
 });
